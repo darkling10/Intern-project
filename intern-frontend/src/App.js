de7cea2c95ee0cb3b8d64b5359ae9a286a7f7c1d
darkling10 <pathan.abbas10@gmail.com>
2022-09-06 19:04:07 +0530

    Route defined

--- intern-frontend/src/App.js
+++ intern-frontend/src/App.js
@@ -1,23 +1,20 @@
-
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import Home from "../src/components/Home";
+import Login from "../src/components/Login";
+import Registration from "../src/components/Registration";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button>He</Button>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+      </Routes>
     </div>
   );
 }
---
+++ intern-frontend/src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      <h1>Hiiiiiiiiiiii</h1>
+      {console.log("In login")}
+    </div>
+  )
+}
+
+export default Login
